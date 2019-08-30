ad779eadaeeca648198fdb2aec9ee3c284244375
Caroline Xiong <CarolineXiong@Carolines-MacBook-Air.local>
2019-08-30 14:45:31 -0700

    edited files

--- pictureTranslate.py
+++ pictureTranslate.py
@@ -1,20 +1,6 @@
 import cv2
 import numpy
 
-cam = cv2.VideoCapture(0)
-
-while True:
-    ret, frame = cam.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame', frame)
-    cv2.imshow('gray', gray)
-
-    if cv2.waitKey(0) & 0xFF == ord('q'):
-        break
-
-cam.release()
-cv2.destroyAllWindows()
-
 def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
@@ -25,3 +11,32 @@ cifar = dict()
 for i in range(1,6):
     cifar.update(unpickle("cifar-10-batches-py/data_batch_%d" % i))
 #print(type(cifar[b'data']))
+
+cam = cv2.VideoCapture(0)
+cv2.namedWindow("frame")
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    cv2.imshow('frame', frame)
+    if not ret:
+        break
+    k = cv2.waitKey(1)
+
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+
+cv2.destroyAllWindows()
+
+
+
