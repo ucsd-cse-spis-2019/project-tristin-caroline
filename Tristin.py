e6756292c31a2797513a0f0226a7bf72bf704e54
tristinxie <tristin@DESKTOP-LOBQKUM.localdomain>
2019-09-03 09:38:11 -0700

    added facial detection

--- Tristin.py
+++ Tristin.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+"""
 def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
@@ -63,4 +63,25 @@ for pt in zip(*loc[::-1]):
 
 cv2.imshow('Detected',img_rgb)
 cv2.waitKey(0)
+"""
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
