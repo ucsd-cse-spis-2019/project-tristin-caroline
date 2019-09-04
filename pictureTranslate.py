af28aafe9855337a9b8048a861794babd847971b
tristinxie <tristin@DESKTOP-LOBQKUM.localdomain>
2019-09-04 16:27:05 -0700

    got picture translate to work for smartphones

--- pictureTranslate.py
+++ pictureTranslate.py
@@ -1,42 +1,52 @@
 import cv2
-import numpy
+import numpy as np
+from googletrans import Translator
+import time
+from PIL import ImageFont, ImageDraw, Image
+translator = Translator()
+#face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+iphone_cascade = cv2.CascadeClassifier('iphonexrcascade15stages.xml')
 
-def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+cap = cv2.VideoCapture(0)
 
-cifar = dict()
-for i in range(1,6):
-    cifar.update(unpickle("cifar-10-batches-py/data_batch_%d" % i))
-#print(type(cifar[b'data']))
-
-cam = cv2.VideoCapture(0)
-cv2.namedWindow("frame")
-img_counter = 0
+def labelPic(x,y,w,h,color,objectName,img):
+    cv2.rectangle(img,(x,y),(x+w,y+h),color,2)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img, "Original Word: " + objectName, (0, 30), font, 1, (0,255,255), 3, cv2.LINE_AA)
+    dest, src = "ja", "en"
+    transWord = translator.translate(objectName, dest, src)
+    fontpath = "./simsun.ttc"     
+    font = ImageFont.truetype(fontpath, 32)
+    img_pil = Image.fromarray(img)
+    draw = ImageDraw.Draw(img_pil)
+    draw.text((0, 420),"Translated Word: " + transWord.text, font = font, fill = (0, 255, 0))
+    img = np.array(img_pil)
+    cv2.imshow("res", img)
+    cv2.waitKey(0)
+    cv2.destroyWindow("res")
+    #cv2.putText(img, transWord.text, (120, 300), font, 2, (0,255,0), 5, cv2.LINE_AA)    
 
 while True:
-    ret, frame = cam.read()
-    cv2.imshow("test", frame)
-    if not ret:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #faces = face_cascade.detectMultiScale(gray)
+    iphones = iphone_cascade.detectMultiScale(gray, 8, 8)
+
+    for (x,y,w,h) in iphones:
+        labelPic(x,y,w,h,(255,255,0),"Smartphone",img)
+        #while True:
+            #key = cv2.waitKey(1) or 0xff
+            #if key == ord('c'):
+                #break
+        #for (x,y,w,h) in faces:
+        #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        #roi_gray = gray[y:y+h, x:x+w]
+        #roi_color = img[y:y+h, x:x+w]
+        #eyes = eye_cascade.detectMultiScale(roi_gray)
+        #for (ex,ey,ew,eh) in eyes:
+        #cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
         break
-    k = cv2.waitKey(1)
-
-    if k%256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
-
-cam.release()
-
-cv2.destroyAllWindows()
-
-
-
